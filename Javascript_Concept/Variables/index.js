1e75457aefee49d8b1297cc6566fb3fecd9481cd
Harsh Shastrakar <harshshastrakar011@gmail.com>
2024-02-08 14:08:19 +0530

    Add Variables Questions

---
+++ Javascript_Concept/Variables/index.js
@@ -0,0 +1,32 @@
+/*
+function test() {
+  let a = " Hello";
+
+  if (true) {
+    let a = "Hi";
+    console.log(a);
+  }
+
+  console.log(a);
+}
+
+test();
+*/
+
+// --------------------------------------------------------------
+
+/*
+function test() {
+  var a = " Hello";
+  let b = " by";
+
+  if (true) {
+    let a = "Hi";
+    var b = "GoodBy";  //refernce error
+    console.log(a);
+  }
+
+  console.log(a);
+}
+test();
+*/
